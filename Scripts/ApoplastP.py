8414c0ea35dbd7390eccb44523bbc259957faea7
Darcy Jones <darcy.ab.jones@gmail.com>
2020-05-26 16:42:59 +0800

    -o flag caused issue because tried to write str to bytes handle.

--- Scripts/ApoplastP.py
+++ Scripts/ApoplastP.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
     ApoplastP: prediction of effectors and plant proteins in the apoplast using machine learning
-    Copyright (C) 2017-2018 Jana Sperschneider	
+    Copyright (C) 2017-2018 Jana Sperschneider
 
-    This program is free software; you can redistribute it and/or modify  
-    it under the terms of the GNU General Public License as published by  
-    the Free Software Foundation; either version 3 of the License, or     
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -47,7 +47,7 @@ def get_weka_path(script_path):
         print("Alternatively set the environment variable 'WEKA38' to point to the folder containing the weka.jar file.")
         print()
         sys.exit(1)
-    
+
     return weka_path
 
 
@@ -112,7 +112,7 @@ def main():
     # -----------------------------------------------------------------------------------------------------------
     # Check if FASTA file exists
     try:
-        open(FASTA_FILE, 'r') 
+        open(FASTA_FILE, 'r')
     except OSError as e:
         print("Unable to open FASTA file:", FASTA_FILE)  #Does not exist OR no read permissions
         print("I/O error({0}): {1}".format(e.errno, e.strerror))
@@ -121,7 +121,7 @@ def main():
     # Try to create folder where results will be stored
     try:
         os.mkdir(RESULTS_PATH)
-    except OSError as exception:        
+    except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
     # -----------------------------------------------------------------------------------------------------------
@@ -145,7 +145,7 @@ def main():
     # -----------------------------------------------------------------------------------------------------------
     # Call pepstats
     ProcessExe = PEPSTATS_PATH + 'pepstats'
-    ParamList = [ProcessExe, '-sequence', f_output, 
+    ParamList = [ProcessExe, '-sequence', f_output,
               '-outfile', RESULTS_PATH + 'pepstats.out']
     try:
         Process = subprocess.Popen(ParamList, shell=False)
@@ -165,7 +165,7 @@ def main():
     # -----------------------------------------------------------------------------------------------------------
     # Parse pepstats file
     pepstats_dic = functions.pepstats(SHORT_IDENTIFIERS, SEQUENCES, RESULTS_PATH + 'pepstats.out')
-    # -----------------------------------------------------------------------------------------------------------   
+    # -----------------------------------------------------------------------------------------------------------
     # Write the WEKA arff file for classification of the input FASTA file
     weka_input = RESULTS_PATH + 'weka.arff'
     functions.write_weka_input(weka_input, SHORT_IDENTIFIERS, SEQUENCES, pepstats_dic)
@@ -195,18 +195,18 @@ def main():
     # Parse the WEKA output file
     file_input = RESULTS_PATH + 'APOPLAST_Predictions.txt'
     predicted_apoplast, predictions = functions.parse_weka_output(file_input, ORIGINAL_IDENTIFIERS, ORIGINAL_SEQUENCES, prob_threshold)
-    # -----------------------------------------------------------------------------------------------------------    
+    # -----------------------------------------------------------------------------------------------------------
     # If user wants the stdout output directed to a specified file
     if output_file:
 
-        with open(output_file, 'wb') as out:
+        with open(output_file, 'w') as out:
             # Short format: output predictions for all proteins as tab-delimited table
             if short_format:
                 out.writelines(functions.short_output(predictions))
             # If the user wants to see the long format, output additional information and stats
             else:
                 out.writelines(functions.short_output(predictions))
-                out.writelines(functions.long_output(ORIGINAL_IDENTIFIERS, predicted_apoplast)) 
+                out.writelines(functions.long_output(ORIGINAL_IDENTIFIERS, predicted_apoplast))
         print('ApoplastP results were saved to output file:', output_file)
 
     else:
@@ -223,17 +223,17 @@ def main():
         with open(apoplast_output, 'w') as f_output:
             for apoplast, prob, sequence in predicted_apoplast:
                 f_output.writelines('>' + apoplast + ' | Apoplast probability: ' + str(prob) + '\n')
-                f_output.writelines(sequence + '\n')  
+                f_output.writelines(sequence + '\n')
     if nonapoplast_output:
         with open(nonapoplast_output, 'w') as f_output:
             for apoplast, pred, prob, sequence in predictions:
                 if pred == 'Non-apoplastic':
                     f_output.writelines('>' + apoplast + ' | Non-apoplast probability: ' + str(prob) + '\n')
-                    f_output.writelines(sequence + '\n')  
+                    f_output.writelines(sequence + '\n')
     # -----------------------------------------------------------------------------------------------------------
     # Clean up and delete temporary folder that was created
     shutil.rmtree(RESULTS_PATH)
-    # -----------------------------------------------------------------------------------------------------------    
+    # -----------------------------------------------------------------------------------------------------------
     try:
         sys.stdout.close()
     except:
@@ -244,7 +244,7 @@ def main():
         pass
     # -----------------------------------------------------------------------------------------------------------
     # -----------------------------------------------------------------------------------------------------------
-    # -----------------------------------------------------------------------------------------------------------    
+    # -----------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     main()
